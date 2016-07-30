dfbe36894a9b23c442a7cee2aa6221924c280a57
Wesley <wesley.nmtorres@gmail.com>
2016-07-30 16:45:04 -0300

    tests running with maven '<build>'

--- src/test/java/daca/jbosswildfly/git/LoginControllerIT.java
+++ src/test/java/daca/jbosswildfly/git/LoginControllerIT.java
@@ -1,11 +1,18 @@
 package daca.jbosswildfly.git;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.jayway.restassured.RestAssured;
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 import bootwildfly.Application;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -13,13 +20,26 @@ import bootwildfly.Application;
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
 public class LoginControllerIT {
-	private static final String CHECKED_FIELD = "checked";
-	private static final String DESCRIPTION_FIELD = "description";
-	private static final String ITEMS_RESOURCE = "/items";
-	private static final String ITEM_RESOURCE = "/items/{id}";
-	private static final int NON_EXISTING_ID = 999;
-	private static final String FIRST_ITEM_DESCRIPTION = "First item";
-	private static final String SECOND_ITEM_DESCRIPTION = "Second item";
-	private static final String THIRD_ITEM_DESCRIPTION = "Third item";
+	private static final String HELLO = "/hello";
+	private static final String LOGIN = "/login";
+	private static final String LOGOUT = "/logout";
+	private static final String ACCOUNT = "/account";
+	private static final String INFO = "/info";
+	private static final String PROBLEM = "/problem";
+	private static final String PROBLEM_ID = "/problem/{id}";
+	private static final String PROBLEM_STATUS = "/problem/{id}/status";
+	private static final String SOLUTION = "/solution";
+	@Value("${local.server.port}")
+	private int serverPort;
+
+	@Before
+	public void setUp() {
+		RestAssured.port = serverPort;
+	}
+
+	@Test
+	public void getItemsShouldReturnBothItems() {
+		when().get(HELLO).then().statusCode(200);
+	}
 
 }
