7320357f149120b26a872a48513999571c94a5aa
Wesley <wesley.nmtorres@gmail.com>
2016-09-04 17:45:17 -0300

    auth getting data from H2

--- src/main/java/bootwildfly/security/SecurityConfig.java
+++ src/main/java/bootwildfly/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package bootwildfly.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,35 +9,74 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
+
+import bootwildfly.models.repositories.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
-	}
-	
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+//	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-        .authorizeRequests()
+		http.authorizeRequests()
+        .antMatchers("/login").permitAll()
         .anyRequest().authenticated()
         .and()
-        .requestCache()
-        .requestCache(new NullRequestCache())
+        .formLogin().permitAll()
         .and()
-        .httpBasic();
+        .csrf().disable();
 	}
 	
+	private CsrfTokenRepository csrfTokenRepository() {
+		  HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		  repository.setHeaderName("X-XSRF-TOKEN");
+		  return repository;
+		}
+
 	@Bean
-    public HttpSessionStrategy httpSessionStrategy() {
-        return new HeaderHttpSessionStrategy();
-    }
+	public HttpSessionStrategy httpSessionStrategy() {
+		return new HeaderHttpSessionStrategy();
+	}
+
+	@Autowired
+	public void REALconfigureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService());
+	}
+
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		return new UserDetailsService() {
+
+			@Autowired
+			UserRepository userRepository;
+
+			@Override
+			public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+				bootwildfly.models.User user = userRepository.findOneByEmail(email);
+				if (user != null) {
+					return new User(user.getEmail(), user.getPassword(), true, true, true, true,
+							AuthorityUtils.createAuthorityList("USER"));
+				} else {
+					throw new UsernameNotFoundException("could not find the user '" + email + "'");
+				}
+			}
+
+		};
+	}
 
 }
