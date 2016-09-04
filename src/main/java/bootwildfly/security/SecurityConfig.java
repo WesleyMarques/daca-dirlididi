532cf55a563c08b56e242afc3b3da5065811a0d3
Wesley <wesley.nmtorres@gmail.com>
2016-09-04 12:54:21 -0300

    auth defaultu withou validate DB

--- src/main/java/bootwildfly/security/SecurityConfig.java
+++ src/main/java/bootwildfly/security/SecurityConfig.java
@@ -1,12 +1,16 @@
 package bootwildfly.security;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -20,8 +24,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http
+        .authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .requestCache()
+        .requestCache(new NullRequestCache())
+        .and()
+        .httpBasic();
 	}
+	
+	@Bean
+    public HttpSessionStrategy httpSessionStrategy() {
+        return new HeaderHttpSessionStrategy();
+    }
 
 }
