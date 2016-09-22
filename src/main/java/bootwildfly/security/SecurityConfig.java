080d3e55de1542e122edbe85255f58aaf7522130
Wesley <wesley.nmtorres@gmail.com>
2016-09-21 22:17:52 -0300

    security without csrf

--- src/main/java/bootwildfly/security/SecurityConfig.java
+++ src/main/java/bootwildfly/security/SecurityConfig.java
@@ -18,7 +18,6 @@ import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilt
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -45,8 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .antMatchers("/*", "/resources/static/**").permitAll()
         .and()
-//        .csrf().disable()
-        .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        .csrf().disable();
+//        .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         http.antMatcher("/logout")
                 //.logout().logoutSuccessUrl("/").permitAll().and()
                 .antMatcher("/**")
